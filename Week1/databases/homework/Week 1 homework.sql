afec6353e4eda6af34180dd892427484094cc6ef
unknown <martinaoyhenard@gmail.com>
2019-10-16 11:12:12 +0200

    added ;

--- Week1/databases/homework/Week 1 homework.sql
+++ Week1/databases/homework/Week 1 homework.sql
@@ -1,10 +1,10 @@
  SELECT Name FROM country WHERE population > 8000000; 
- SELECT * FROM country WHERE name LIKE '%land'; 
- SELECT * FROM city WHERE population > 500000 AND population < 1000000; 
- SELECT * FROM country WHERE Continent = 'Europe'; 
- SELECT * FROM country ORDER BY SurfaceArea DESC;  
- SELECT city.Name from city JOIN country ON city.CountryCode = country.Code WHERE country.Name = 'Netherlands'
+ SELECT Name FROM country WHERE name LIKE '%land'; 
+ SELECT Name FROM city WHERE population > 500000 AND population < 1000000; 
+ SELECT Name FROM country WHERE Continent = 'Europe'; 
+ SELECT Name FROM country ORDER BY SurfaceArea DESC;  
+ SELECT city.Name from city INNER JOIN country ON city.CountryCode = country.Code WHERE country.Name = 'Netherlands';
  SELECT population FROM city WHERE name = 'Rotterdam'; 
- SELECT * FROM country ORDER BY SurfaceArea DESC LIMIT 10;  
- SELECT * FROM city ORDER BY population DESC LIMIT 10; 
+ SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10;  
+ SELECT Name FROM city ORDER BY population DESC LIMIT 10; 
 SELECT SUM(population) FROM city;  
