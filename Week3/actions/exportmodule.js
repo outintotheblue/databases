5ecebdd8863ecbe952363554a913427b7cdd631b
unknown <martinaoyhenard@gmail.com>
2019-11-10 18:28:46 +0100

    created tables

--- Week3/actions/exportmodule.js
+++ Week3/actions/exportmodule.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    readTodos:  require('./readTodos'),
-      insertTodo: require('./insertTodo'),
+  readTodos:  require('./readTodos'),
+  insertTodo: require('./insertTodo'),
   deleteTodo: require('./deleteTodo'),
   deleteTodos: require('./deleteTodos'),
   markAsCompleted: require('./markAsCompleted'),
